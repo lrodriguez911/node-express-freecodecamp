1e3a662bfd02b3459210556de451556161d67602
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-08-20 19:58:22 -0300

    add module console

--- practice-node/app.js
+++ practice-node/app.js
@@ -9,3 +9,20 @@ console.log(saludar.saludar('lucas'))
 
 //usando desestructuracion
 console.log(saludarHolaMundo('lucas'))
+
+//modulos built-in (incorporados sin necesidad de instalar o requerir(importar))
+// http, https, fs(file-system), os(operating system), path(link, way, route)
+
+//modulo console
+console.log('Hola console')
+
+console.warn('advertencia')
+
+console.error(new Error('ocurrio un error'));
+
+//console.assert(1 > 0, 'hola')
+
+//console.table()
+
+
+//modulo process
