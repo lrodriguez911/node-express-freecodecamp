eef3e475a3bf1f0c673c16e3d7ba357be47c30b6
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-09-11 21:40:58 -0300

    learning about module process

--- practice-node/app.js
+++ practice-node/app.js
@@ -1,24 +1,24 @@
 //se puede importar usando require
-const saludar = require('./saludos.js')
+/* const saludar = require('./saludos.js') */
 // se puede importar unando desestructuracion de objs para llamar directamente el metodo
-const {saludarHolaMundo} = require('./saludos.js')
+/* const {saludarHolaMundo} = require('./saludos.js') */
 
 
 //sin usar desestructuracion
-console.log(saludar.saludar('lucas'))
+/* console.log(saludar.saludar('lucas')) */
 
 //usando desestructuracion
-console.log(saludarHolaMundo('lucas'))
+/* console.log(saludarHolaMundo('lucas')) */
 
 //modulos built-in (incorporados sin necesidad de instalar o requerir(importar))
 // http, https, fs(file-system), os(operating system), path(link, way, route)
 
 //modulo console
-console.log('Hola console')
+/* console.log('Hola console')
 
 console.warn('advertencia')
 
-console.error(new Error('ocurrio un error'));
+console.error(new Error('ocurrio un error')); */
 
 //console.assert(1 > 0, 'hola')
 
@@ -26,3 +26,14 @@ console.error(new Error('ocurrio un error'));
 
 
 //modulo process
+
+/* console.log(process)
+
+console.log(process.env) */
+
+/* console.log(process.argv)
+for (const i of process.argv) {
+    console.log(i)
+} */
+
+console.log(process.memoryUsage());
