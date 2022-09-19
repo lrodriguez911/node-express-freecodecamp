62d655f320ac290a6caa1e83911e0852717b560a
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-09-18 23:00:44 -0300

    practice whit module fs

--- practice-node/fs.js
+++ practice-node/fs.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require("fs");
 
 /* modulo FS (file system) todos los metodos son asincronos por defecto
 si quieres modicar usan versiones sincronas agregando Sync como sufijo*/
@@ -7,12 +7,62 @@ si quieres modicar usan versiones sincronas agregando Sync como sufijo*/
 
 /* se puede leer, modificacr copiar eliminar cambiar nombre */
 
-fs.readFile('index.html', 'utf-8', (err, done) =>{
-	if(err) throw err;
-	console.log(done)
+/* leer el archivo usando la codificacion elegida */
+
+console.log('antes de leer');
+
+/* fs.readFileSync("index.html", "utf-8", (err, done) => {
+  if (err) throw err;
+  console.log(done);
+}); */
+/* las versiones Sync no necesitan la callback xq se van ejecutar de manera sincrona */
+const archivo = fs.readFileSync('index.html', 'utf-8')
+
+console.log(archivo);
+console.log('despues de leer');
+
+/* cambiar nombre */
+
+console.log('antes de rename');
+
+fs.renameSync("index.html", "inde.html", (err) => {
+  if (err) throw err;
 });
 
-fs.rename('index.html', 'inde.html', (err, done) =>{
-    if(err) throw err;
-    return done
-})
+console.log('despues de rename');
+
+/* agregar contenido al final dearchivo con append */
+
+console.log('antes de append');
+
+fs.appendFileSync("index.html", "<p>Hola</p>", (err) => {
+  if (err) {
+    throw err;
+  }
+});
+
+console.log('despues de append');
+
+/* reemplazar todo el contenido del archivo */
+
+console.log('antes de write');
+
+fs.writeFileSync("index.html", "nuevo contenido", (err) => {
+  if (err) {
+    throw err;
+  }
+});
+console.log('despues de write');
+
+/* eliminar archivo */
+
+console.log('antes de unlink');
+
+fs.unlinkSync("index.html", (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("archivo eliminado");
+});
+
+console.log('despues de unlink');
