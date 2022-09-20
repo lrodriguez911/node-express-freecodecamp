768ab9f7d941d90b936c94ac911d85cc2ea5b6a7
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-09-20 00:07:02 -0300

    add modile eventsemitter

---
+++ practice-node/practice-events/app.js
@@ -0,0 +1,9 @@
+const EventEmitter = require('events');
+
+const emisorProductos = new EventEmitter();
+
+emisorProductos.on('compra', ()=> {
+  console.log('Se realizo una compra');
+})
+
+/* emisorProductos.emit('compra'); */
