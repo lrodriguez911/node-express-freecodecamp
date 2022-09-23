7e2b0f60039d0b693e348d77a8f73a6f4290e309
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-09-23 14:30:13 -0300

    add practice whit promises

--- practice-node/practice-promises/app.js
+++ practice-node/practice-promises/app.js
@@ -1,60 +1,39 @@
-/* const promesaCumplida  = true;
+function ordenarProducto(producto) {
+    return new Promise((resolve, reject) => {
+        console.log(`Ordenando el ${producto}`);
+        setTimeout(() => {
+            if (producto === 'taza') {
+                resolve('ordenando Producto')
+            } else {
+                reject('Producto no disponible')
+            }
+        }, 2000);
+    });
+}
 
-const miPromesa = new Promise((resolve, reject) => {
-    setTimeout(()=>{
-        if(promesaCumplida){
-            resolve('Promesa Cumplida')
-        }else{
-            reject('Promesa rechazada')
-        }
-    }, 2000)
+function procesarPedido(respuesta) {
+    return new Promise((reso, reject) => {
+        console.log('Procesando respuesta ...');
+        console.log(`La respuesta fue "${respuesta}"`);
+        setTimeout(() => {
+            reso('Gracias por su compra. Disfrute su producto')
+        }, 4000);
+    })
+}
+
+
+/* ordenarProducto('lapiz')
+.then(respuesta => {
+    console.log('Respuesta recibida');
+    console.log(respuesta);
+    return procesarPedido(respuesta)
 })
+.then(respuestaProcesada => {
+    console.log(respuestaProcesada);
+}).catch(err => {
+    throw console.log(err);
+}) */
 
-const manejarPromesaCumplida = (value) => {
-    console.log(value);
-}
-
-const manejarPromesaRechazada = (razonRechazo) => {
-    console.log(razonRechazo);
-}
-miPromesa.then(manejarPromesaCumplida, manejarPromesaRechazada) */
+/* using async await */
 
 
-const estatusPedido = ()=>{
-    const estatus = Math.random() < 0.8;
-    console.log(estatus);
-    return estatus;
-}
-/* for (let i = 0; i < 10; i++) {
-    estatusPedido()
-} */
-const miPedidoDePizza = new Promise((resolve, reject) => {
-    setTimeout(()=>{
-        if (estatusPedido()) {
-            resolve('Pizza en camino')
-        } else {
-            reject('Su pedido fue rechazado por falta de fondos')
-        }
-    },1000)
-})
-
-/* const manejarPedido = (mensajeDeConfirmacion) => {
-    console.log(mensajeDeConfirmacion);
-}
-const rechazarPedido = (mensajeDeRechazo) => {
-    console.log(mensajeDeRechazo);
-}
-miPedidoDePizza.then(manejarPedido, rechazarPedido) */
-
-/* miPedidoDePizza.then((mensajeDeConfirmacion) => {
-    console.log(mensajeDeConfirmacion)}).then(null,(mensajeDeRechazo) => {
-        console.log(mensajeDeRechazo);
-    } ) */
-
-miPedidoDePizza.then((mensajeDeConfirmacion)=>{
- console.log(mensajeDeConfirmacion);
-}).catch((mensajeDeRechazo) => {
-    console.log(mensajeDeRechazo);
-})
-
-/* miPedidoDePizza.then(manejarPedido).catch(rechazarPedido) */
