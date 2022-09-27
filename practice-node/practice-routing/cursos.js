5cfdc73d2bea8e1e5bf56200eeed25dcc5b8b551
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-09-27 00:37:04 -0300

    add rounting

---
+++ practice-node/practice-routing/cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+  programacion: [
+    {
+      id: 1,
+      titulo: "Aprende Python",
+      lenguaje: "python",
+      vistas: 10000,
+      nivel: "basico",
+    },
+    {
+      id: 2,
+      titulo: "Pyhton intermedio",
+      lenguaje: "python",
+      vistas: 12000,
+      nivel: "intermedio",
+    },
+    {
+      id: 3,
+      titulo: "Aprende JavaScript",
+      lenguaje: "javascript",
+      vistas: 102000,
+      nivel: "basico",
+    },
+  ],
+  matematicas: [
+    {
+      id: 1,
+      titulo: "Aprende Calculo",
+      tema: "calculo",
+      vistas: 9500,
+      nivel: "basico",
+    },
+    {
+      id: 2,
+      titulo: "Aprende Algebra",
+      tema: "algebra",
+      vistas: 5000,
+      nivel: "intermedio",
+    },
+  ],
+};
+
+module.exports.infoCursos = infoCursos
