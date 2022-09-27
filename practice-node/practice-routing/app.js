ba1bf91dec86bec209f082bccddbd22cfa5de4e1
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-09-27 11:17:48 -0300

    add new routing

--- practice-node/practice-routing/app.js
+++ practice-node/practice-routing/app.js
@@ -1,45 +1,56 @@
-const http = require('http')
-const PORT = 3000
-const cursos = require('./cursos')
+const http = require("http");
+const PORT = 3000;
+const cursos = require("./cursos");
 
 function manejarMethodGET(req, res) {
-    const path = req.url;
-    if(path === '/'){
-        res.statusCode = 200;
-        res.end('Bienvenidos a mi server y API creado con NODE.JS')
-    } else if(path === '/cursos'){
-        res.statusCode = 200;
-        res.end(JSON.stringify(cursos.infoCursos))
-    }else if(path === '/cursos/programacion'){
-        res.statusCode = 200;
-        res.end(JSON.stringify(cursos.infoCursos.programacion))
-    }else if(path === '/cursos/matematicas'){
-        res.statusCode = 200;
-        res.end(JSON.stringify(cursos.infoCursos.matematicas))
-    }
+  const path = req.url;
+
+  if (path === "/") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    //res.statusCode = 200;
+    return res.end(
+      JSON.stringify("Bienvenidos a mi server y API creado con NODE.JS"),
+    );
+  } else if (path === "/cursos") {
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos.infoCursos));
+  } else if (path === "/cursos/programacion") {
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos.infoCursos.programacion));
+  } else if (path === "/cursos/matematicas") {
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos.infoCursos.matematicas));
+  }
+  res.statusCode = 404;
+  console.log(res.statusCode);
+  return res.end("El recurso solicitado no existe");
 }
 
 function manejarMethodPOST(req, res) {
-    const path = req.url;
-    if (path === '/cursos/programacion') {
-        res.statusCode = 200;
-        res.end(`El server recibio una solicitud POST en ${path}`)
-    }
+  const path = req.url;
+  if (path === "/cursos/programacion") {
+    res.statusCode = 200;
+    return res.end(`El server recibio una solicitud POST en ${path}`);
+  }
+  res.statusCode = 404;
+  res.end("No se pudo guardar la solicitud");
 }
 const server = http.createServer((req, res) => {
-    const {method} = req;
-    switch (method) {
-        case 'GET':
-            return manejarMethodGET(req, res);
-        case 'POST':
-            return manejarMethodPOST(req, res);
-        /* case 'PUT':
-            
-            break; */
-        default:
-            console.log(`el metodo usado no puede ser manejado por el servidor ${method}`);
-    }
-})
-server.listen(PORT, ()=>{
-    console.log(`El server esta escuchando en el puerto: ${PORT}`);
-})
+  const { method } = req;
+  switch (method) {
+    case "GET":
+      return manejarMethodGET(req, res);
+    case "POST":
+      return manejarMethodPOST(req, res);
+    case "DELETE":
+      break;
+    default:
+      res.statusCode = 501;
+      res.end(
+        `el metodo usado no puede ser manejado por el servidor ${method}`,
+      );
+  }
+});
+server.listen(PORT, () => {
+  console.log(`El server esta escuchando en el puerto: ${PORT}`);
+});
