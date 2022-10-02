b883e939c8854f1a10677ac04c3fe0da5912dc66
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-10-01 21:41:58 -0300

    using req.params

--- practice-node/practice-express/app.js
+++ practice-node/practice-express/app.js
@@ -3,4 +3,21 @@ const express = require('express');
 const app = express();
 const {infoCursos} = require('./cursosDB.js')
 
-app.get('/', (req, res) => res.json({"hola": "saludos"}))
+app.get('/', (req, res) => res.send('Hola a todos usando res.send saludos a todos.'))
+
+app.get('/api/cursos', (req, res)=>{res.send(JSON.stringify(infoCursos))})
+
+app.get('/api/cursos/programacion', (req, res)=>{res.send(JSON.stringify(infoCursos.programacion))})
+
+app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
+  const {lenguaje} = req.params
+  res.send(`No hay cursos de ${lenguaje}`)
+})
+
+app.get('/api/cursos/matematicas', (req, res)=>{res.send(JSON.stringify(infoCursos.matematicas))})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`server listening in ${PORT}`);
+})
